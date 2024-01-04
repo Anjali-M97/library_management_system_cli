63d8ae8fa8270ebc4693d168cde2ceb10c0f15e9
anjali <aceava423@gmail.com>
2024-01-04 18:12:25 +0545

    add, delete, find books functions is working

--- db.py
+++ db.py
@@ -19,6 +19,31 @@ def fetch_book():
     close_connection(con)
     return rows
 
+def find_book(title, author):
+    con = connect_to_db()
+    cur = con.cursor()
+    if title!=None and author!=None:
+        cur.execute('SELECT * FROM books WHERE title = ? and author = ?', (title, author))
+    elif title!=None:
+        cur.execute('SELECT * FROM books WHERE title = ? ', (title,))
+    elif author!=None:
+        cur.execute('SELECT * FROM books WHERE author = ?', (author))
+    else:
+        cur.execute('SELECT * FROM books')
+
+    rows = cur.fetchall()
+    close_connection(con)
+    return rows
+
+def delete_book(title, author):
+    con = connect_to_db()
+    cur = con.cursor()
+    cur.execute('DELETE FROM books WHERE title = ? and author = ?', (title, author))
+    close_connection(con)
+
+def update_book():
+    pass
+
 
 def close_connection(con):
     con.commit()
--- index.py
+++ index.py
@@ -1,14 +1,20 @@
 import click
 import db
 
-@click.command()
+@click.group()
+def cli():
+    pass
+
+@cli.command()
 @click.option('--author', '--a', prompt='Author of the Book')
 @click.option('--title', '--t', prompt='Title of Book: ')
 @click.option('--amount', '--n', prompt='Total no. of Book: ')
 def add_book(author, title, amount):
     """Add a Book"""
-    db.insert_book(author, title, amount)
-    
+    db.insert_book(title, author, amount)
+
+@cli.command()
+def view_books():
     data = db.fetch_book()
 
     # Print the fetched data
@@ -16,5 +22,23 @@ def add_book(author, title, amount):
     for row in data:
         print(row)
 
+@cli.command()
+@click.option('--author', '--a', help='Author to Search', default=None )
+@click.option('--title', '--t', help='Book to Search', default=None)
+def search_book(author, title):
+    """Search a Book"""
+    data = db.find_book(title, author)
+
+    print("Data in the table:")
+    for row in data:
+        print(row)
+
+@cli.command()
+@click.option('--author', '--a', prompt='Author of the Book')
+@click.option('--title', '--t', prompt='Title of Book: ')
+def remove_book(author, title):
+    """Remove a Book"""
+    db.delete_book(title, author)
+
 if __name__ == '__main__':
-    add_book()
+    cli()
